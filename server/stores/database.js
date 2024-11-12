55f65508e238da900bdf09c72596187e7a1598ce
Kolton <kguthrie2@stu.jsu.edu>
2024-11-12 14:03:07 -0600

    Added getAllRatesByDate

--- server/stores/database.js
+++ server/stores/database.js
@@ -1,8 +1,25 @@
 import sqlite3 from "sqlite3";
 
+const SELECT_RATE_BY_DATE = "SELECT * FROM rate WHERE date = ?;";
+
 let db = new sqlite3.Database("./stores/currency.db", sqlite3.OPEN_READWRITE, (err) => {
 	if (err) {
 		console.error(err.message);
 	}
 	console.log("Connected to the database!");
 });
+
+function getAllRatesByDate(date, cb) {
+    db.all(SELECT_RATE_BY_DATE, [date], (err, rows) => {
+		if (err) {
+			console.error(err.message);
+			cb(err, null);
+		} else {
+			cb(null, rows);
+		}
+    });
+}
+
+export {
+    getAllRatesByDate
+};
