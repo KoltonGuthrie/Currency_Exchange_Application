40496f559741bd8865acd71c75bfe46f4ea9fb6c
Kolton Guthrie <guthriekolton@gmail.com>
2024-11-12 19:22:42 -0600

    Added formatDate util

---
+++ server/utils/format_date.js
@@ -0,0 +1,5 @@
+export function formatDate(date) {
+    const offset = date.getTimezoneOffset()
+    date = new Date(date.getTime() - (offset*60*1000))
+    return date.toISOString().split('T')[0];
+}
