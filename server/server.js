ac1835f91489a8fbe2fbc9059d6cabcd4b164a59
Kolton Guthrie <guthriekolton@gmail.com>
2024-11-12 19:22:06 -0600

    Added conversion and isCurrency checks to get request

--- server/server.js
+++ server/server.js
@@ -1,11 +1,12 @@
 import express from "express";
-import cors from 'cors'
+import cors from "cors";
 import * as Database from "./stores/database.js";
+import { formatDate } from "./utils/format_date.js";
 
 const app = express();
 app.use(express.json());
 app.use(cors());
-app.use('/', express.static('public'));
+app.use("/", express.static("public"));
 
 /*
 
@@ -29,34 +30,63 @@ https://api.apilayer.com/exchangerates_data/convert?to={to}&from={from}&amount={
 }
 */
 
-app.get('/api/exchange/convert', (req, res) => {
-    const result = {success: false, error: "Internal Server Error."};
+app.get("/api/exchange/convert", (req, res) => {
+	const result = { success: false, error: "Internal Server Error." };
+	res.setHeader("Content-Type", "application/json");
 
-    console.log(req.query)
+	const CONVERT_TO = req.query.to;
+	const CONVERT_FROM = req.query.from;
+	const CONVERT_AMOUNT = req.query.amount;
+	const CONVERT_DATE = req.query.date || formatDate(new Date());
 
-    const CONVERT_TO = req.query.to;
-    const CONVERT_FROM = req.query.from;
-    const CONVERT_AMOUNT = req.query.amount;
-    const CONVERT_DATE = req.query.date || new Date().toISOString().split('T')[0];
+	if (!CONVERT_TO || !CONVERT_FROM || !CONVERT_AMOUNT || !CONVERT_DATE) {
+		result.error = "Query 'to', 'from' and 'amount' are required!";
+		return res.send(result);
+	}
 
-    if(!CONVERT_TO || !CONVERT_FROM || !CONVERT_AMOUNT || !CONVERT_DATE) {
-        result.error = "Query 'to', 'from' and 'amount' are required!";
-        return res.send(result);
-    }
+	Database.isCurrency(CONVERT_TO, (IS_CUR) => {
+		if (!IS_CUR) {
+			result.error = CONVERT_TO + " is not a value currency!";
+			return res.send(result);
+		}
 
-    result.success = true;
-    result.error = undefined;
-    result.query = {
-        to: CONVERT_TO,
-        from: CONVERT_FROM,
-        amount: CONVERT_AMOUNT,
-        date: req.query.date
-    }
-    result.date = CONVERT_DATE;
+		Database.isCurrency(CONVERT_FROM, (IS_CUR) => {
+			if (!IS_CUR) {
+				result.error = CONVERT_FROM + " is not a value currency!";
+				return res.send(result);
+			}
 
-    res.send(result)
+			Database.getConversionRateToFrom({ date: CONVERT_DATE, to: CONVERT_TO, from: CONVERT_FROM }, (err, row) => {
+				if (err) {
+					console.error(err);
+					result.error = err.message;
 
+					return res.send(result);
+				}
 
+				if (!row) {
+					result.error = "Failed to find conversion... This should NEVER happen!";
+
+					return res.send(result);
+				}
+
+				result.success = true;
+				result.error = undefined;
+				result.query = {
+					to: CONVERT_TO,
+					from: CONVERT_FROM,
+					amount: CONVERT_AMOUNT,
+				};
+				result.info = {
+					rate: row.converted_rate,
+				};
+				result.date = CONVERT_DATE;
+				result.result = row.converted_rate * CONVERT_AMOUNT;
+
+				return res.send(result);
+			});
+		});
+	});
 });
 
 export default app;
