ff8287449168032b4d54cdcca1b916f06585341f
Kolton Guthrie <guthriekolton@gmail.com>
2024-11-12 21:36:26 -0600

    Added getExchangeRates util

---
+++ server/utils/get_exchange_rates.js
@@ -0,0 +1,34 @@
+import http from 'node:http';
+import { formatDate } from "./format_date.js";
+
+export function getExchangeRates(date = formatDate(new Date()), cb) {
+	const getOptions = {
+		hostname: "api.apilayer.com",
+		path: "/exchangerates_data/" + date,
+		method: "GET",
+		headers: {
+			"Content-Type": "application/json",
+            "apiKey": process.env.API_KEY,
+		},
+	};
+
+	const req = http.request(getOptions, (res) => {
+		let chunks = [];
+		console.log(`Status: ${res.statusCode}`);
+		res.setEncoding("utf8");
+		res.on("data", (chunk) => {
+			chunks.push(chunk);
+		});
+		res.on("end", () => {
+			let json = JSON.parse(chunks.join(''));
+			return cb(null, json);
+		});
+	});
+
+    req.on('error', (err) => {
+        console.error(`Error: ${e.message}`);
+        return cb(err.message, null);
+    });
+
+    req.end();
+}
