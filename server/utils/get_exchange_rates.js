88f4749f3d79e47d2378f246cc756125dc357ee0
Kolton <kguthrie2@stu.jsu.edu>
2024-11-19 13:22:17 -0600

    Added getExchangeSymbols

--- server/utils/get_exchange_rates.js
+++ server/utils/get_exchange_rates.js
@@ -32,3 +32,35 @@ export function getExchangeRates(date = formatDate(new Date()), cb) {
 
 	req.end();
 }
+
+export function getExchangeSymbols(cb) {
+	const getOptions = {
+		hostname: "api.apilayer.com",
+		path: "/exchangerates_data/symbols",
+		method: "GET",
+		headers: {
+			"Content-Type": "application/json",
+			apiKey: process.env.API_KEY,
+		},
+	};
+
+	const req = http.request(getOptions, (res) => {
+		let chunks = [];
+		console.log(`Status: ${res.statusCode}`);
+		res.setEncoding("utf8");
+		res.on("data", (chunk) => {
+			chunks.push(chunk);
+		});
+		res.on("end", () => {
+			let json = JSON.parse(chunks.join(""));
+			return cb(null, json);
+		});
+	});
+
+	req.on("error", (err) => {
+		console.error(`Error: ${e.message}`);
+		return cb(err.message, null);
+	});
+
+	req.end();
+}
