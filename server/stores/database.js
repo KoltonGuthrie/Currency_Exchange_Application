903c3128e93d4d186c5f64d98eba060eac6e094d
Kolton <kguthrie2@stu.jsu.edu>
2024-11-19 13:22:55 -0600

    Database will now populate if the .db file does not exist or if currency table is empty

--- server/stores/database.js
+++ server/stores/database.js
@@ -1,6 +1,10 @@
 import sqlite3 from "sqlite3";
 import { formatDate } from "../utils/format_date.js";
-import { getExchangeRates } from "../utils/get_exchange_rates.js";
+import { getExchangeRates, getExchangeSymbols } from "../utils/get_exchange_rates.js";
+
+const CREATE_CURRENCY_TABLE = "CREATE TABLE IF NOT EXISTS currency (_id text primary key, description text);";
+
+const CREATE_RATE_TABLE = "CREATE TABLE IF NOT EXISTS rate (currency_id text, date date, rate float, foreign key (currency_id) references currency (_id), primary key (currency_id, date));";
 
 const SELECT_RATE_BY_DATE = "SELECT * FROM rate WHERE date = ?;";
 
@@ -14,17 +18,65 @@ const CONVERT_TO_FROM = `
 
 const SELECT_CURRENCY_BY_ID = "SELECT * FROM currency WHERE _id = ?;";
 
-const INSERT_PARTIAL_QUERY = "INSERT INTO rate (currency_id, rate, date) VALUES ";
+const INSERT_CURRENCY_PARTIAL_QUERY = "INSERT INTO currency (_id, description) VALUES ";
+
+const INSERT_RATE_PARTIAL_QUERY = "INSERT INTO rate (currency_id, rate, date) VALUES ";
 
 const SELECT_CONVERSION_DATE = "SELECT * FROM rate WHERE date = ? LIMIT 1;";
 
 const SELECT_ALL_CURRENCY = "SELECT * FROM currency;";
 
-let db = new sqlite3.Database("./stores/currency.db", sqlite3.OPEN_READWRITE, (err) => {
+let db = new sqlite3.Database("./stores/currency.db", sqlite3.OPEN_READWRITE | sqlite3.OPEN_CREATE, (err) => {
 	if (err) {
 		console.error(err.message);
 	}
 	console.log("Connected to the database!");
+});
+
+db.serialize(() => {
+	db.run(CREATE_CURRENCY_TABLE);
+	db.run(CREATE_RATE_TABLE);
+
+	db.get(SELECT_ALL_CURRENCY, (err, row) => {
+		if(err) throw err;
+
+		if(!row) {
+			// No data in currency. Add symbols
+			getExchangeSymbols((err, data) => {
+				if(err) throw err;
+
+				if(data && data.success) {
+					
+					let arr = [];
+					
+					for (const key of Object.keys(data.symbols)) {
+						const value = data.symbols[key];
+						arr.push([key, value]);
+					}
+				
+					let parameters = [];
+					arr.map((_) => {
+						_.map((el) => parameters.push(el));
+					});
+				
+					let sql = INSERT_CURRENCY_PARTIAL_QUERY + arr.map((_) => "(?, ?)").join(", ");
+				
+					db.run(sql, parameters, function (err) {
+						if (err) {
+							throw err;
+						}
+				
+						console.log("Added " + this.changes + " currency symbols to database");
+					});
+
+
+				}
+
+			});
+		}
+	})
+
+	
 });
 
 function getAllCurrency(cb) {
@@ -71,7 +123,7 @@ function addRates(json, cb) {
 		_.map((el) => parameters.push(el));
 	});
 
-	let sql = INSERT_PARTIAL_QUERY + arr.map((_) => "(?, ?, ?)").join(", ");
+	let sql = INSERT_RATE_PARTIAL_QUERY + arr.map((_) => "(?, ?, ?)").join(", ");
 
 	db.run(sql, parameters, function (err) {
 		if (err) {
