b3fe22b8f20fca029f81d17cc285cc423828caf1
Kolton Guthrie <guthriekolton@gmail.com>
2025-06-05 18:49:44 -0500

    Added delay between starting server and starting client

--- start.bat
+++ start.bat
@@ -5,5 +5,7 @@ call npm run install-dependencies
 echo Starting server...
 start cmd.exe /c "npm run start-server"
 
+TIMEOUT /T 2
+
 echo Starting client...
 start cmd.exe /c "npm run start-client"
